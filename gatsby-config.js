41857d8b5dc0d56937f65e153020511d7be18a73
Joost v Velthoven <joost@crave-emenu.com>
2020-09-04 06:56:25 +0200

    add gatsby-plugin-canonical-urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.englishproficiency.ru`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
